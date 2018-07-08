f16eca8d5571134332f6f14c620916cea564983c
juliorkm <julioramakrsnamandoju@gmail.com>
2018-07-08 16:31:53 -0300

    Média amostral da geométrica
    - Criada função random() que gera números aleatórios entre 0 e 1;
    - Encontrada cdf invertida para a geométrica e implementada usando
    funções log() e ceil();
    - Médias amostrais de ambas as variáveis aleatórias impressas usando uma
    mesma seed;
    - Ambas as médias amostrais estão consistentemente próximas das
    esperadas (755 e 22).

--- rand_test.cpp
+++ rand_test.cpp
@@ -7,10 +7,15 @@
 #include <stdlib.h>
 #define STDLIB
 #endif
+#ifndef MATH
+#include <math.h>
+#define MATH
+#endif
 
 // Function that determines the size of a data package (actual function)
-int data_transfer() {
-	double seed = (double) rand() / RAND_MAX;
+int data_package_size() {
+	long double seed = random();
+	//printf("Seed is %Lf\n", seed);
 	if (seed < .3) return 64;
 	else if (seed < .3 + .1) return 512;
 	else if (seed < .3 + .1 + .3) return 1500;
@@ -18,6 +23,13 @@ int data_transfer() {
 		seed = (seed - .3 - .1 - .3) / (1 - .3 - .1 - .3);
 		return seed * (1500 - 64) + 64;
 	}
+}
+
+// Function that determines the number of voice packages (actual function)
+int voice_package_number() {
+	long double seed = (long double) random();
+	if (seed == 1) seed = .999999; // seed cannot be 1 or there would be a log(0), which is infinite, so... we make it a little smaller
+	return (int) ceil(log(1.0 - seed) / log(1 - 1.0L / 22.0));
 }
 
 // This function only exists to make sure that the percentages are correct
@@ -32,7 +44,7 @@ void test_sample_mean(int (*function)()) {
 		else if (current_sample == 512) n512++;
 		else if (current_sample == 1500) n1500++;
 	}
-	printf("64: %lf%%\n512: %lf%%\n1500: %lf%%\n", (double)n64/BIGNUM, (double)n512/BIGNUM, (double)n1500/BIGNUM);
+	printf("\t\t64: %lf%%\n\t\t512: %lf%%\n\t\t1500: %lf%%\n", (double)n64/BIGNUM * 100, (double)n512/BIGNUM * 100, (double)n1500/BIGNUM * 100);
 }
 
 int main() {
@@ -41,9 +53,14 @@ int main() {
 	scanf("%int", &random);
 	srand(random);
 	
-	printf("\nSample Mean: ");
+	putchar('\n');
 	
-	printf("%lf\n", sample_mean(&data_transfer));
+	printf("Data Package Size\n");
+	printf("\tSample Mean: %lf\n", sample_mean(&data_package_size));
+	test_sample_mean(&data_package_size);
 	
-	test_sample_mean(&data_transfer);
+	putchar('\n');
+	
+	printf("Voice Package Quantity\n");
+	printf("\tSample Mean: %lf\n", sample_mean(&voice_package_number));
 }
